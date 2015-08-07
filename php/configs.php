b95b2af6cd8d43fd3e7b55a564ca5beac5b774ed
sandrogallina <sandrogallina1984@gmail.com>
2015-08-07 09:32:15 -0300

    Update configs.php

--- php/configs.php
+++ php/configs.php
@@ -3,3 +3,5 @@
 define("AMBIENTE","http://api.replay4.me");
 define("CLIENT_ID",0); // ID da empresa
 define("CLIENT_SECRET","xxx"); // TOKEN DA EMPRESA
+ini_set("short_open_tag", true); // Habilita o uso de tags curtas no PHP
+
